1119c92e7b8b2358ec89a1bb4f7fbb125fbe68c5
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2022-10-24 20:03:06 +0300

    Dota: add part of the e2e test of full data flow (make it fail)

--- tests/dota2.test.ts
+++ tests/dota2.test.ts
@@ -1,5 +1,7 @@
 import Dota, {
     SteamID,
+    DotaMatchDetails,
+    DotaPlayerMatchDetails,
 } from "../API/dota2"
 import dotenv from "dotenv"
 
@@ -23,7 +25,7 @@ beforeEach(() => {
 })
 
 describe("Dota", () => {
-    it("should fetch ids of last 100 matches by player id", async () => {
+    it("getPlayerLastMatchesMeta should fetch ids and participation details of last 100 matches by player id", async () => {
         const knownPlayerId = new SteamID({ SteamID64String: "76561198867994061"})
         const result = await instance.getPlayerLastMatchesMeta(knownPlayerId)
 
@@ -31,5 +33,33 @@ describe("Dota", () => {
         expect(result[0]).toBeTruthy()
         expect(result[0].match_id).toBeDefined()
         expect(result[0].player_slot).toBeDefined()
+    })
+
+    it("should allow to get stats before the tournament, after it, and summarize the differences", async () => {
+        const knownPlayerId = new SteamID({ SteamID64String: "76561198867994061"})
+        const matchesMeta = await instance.getPlayerLastMatchesMeta(knownPlayerId)
+
+        /*
+            in real application, these will be the matches played during the tournament, something like:
+            tournamentStart: Date
+            tournamentEnd: Date
+            const filteredMatchesMeta = matchesMeta.filter(({ start_time }) =>
+                start_time * 1000 >= tournamentStart.getTime() &&
+                start_time * 1000 < tournamentEnd.getTime())
+         */
+        const mockedFilteredMatchesMeta = matchesMeta.slice(0, 3)
+
+        // in real application, there may be some queue for requests (TODO: are there limits?)
+        const matchesDetails: (DotaMatchDetails & { player_slot: number })[] = []
+        for(const matchMeta of mockedFilteredMatchesMeta) {
+            const { player_slot } = matchMeta
+            matchesDetails.push({
+                player_slot,
+                ...(await instance.getMatchDetails(matchMeta.match_id))
+            })
+        }
+
+        const isTestFinished = false
+        expect(isTestFinished).toBeTruthy()
     })
 })

5299e9251ee1db0068e223eef1aecfb98a2dc70c
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2022-10-24 19:43:23 +0300

    Dota: implement getMatchDetails (and corresponding interfaces)

--- API/dota2.ts
+++ API/dota2.ts
@@ -28,6 +28,91 @@ interface PlayerParticipationDetails {
     hero_id: number
 }
 
+export interface DotaPlayerMatchDetails {
+    account_id?: number,
+    player_slot: number,
+    team_number: number,
+    team_slot: number,
+
+    hero_id: number,
+    item_0: number,
+    item_1: number,
+    item_2: number,
+    item_3: number,
+    item_4: number,
+    item_5: number,
+    backpack_0: number,
+    backpack_1: number,
+    backpack_2: number,
+    item_neutral: number,
+
+    kills: number,
+    deaths: number,
+    assists: number,
+    last_hits: number,
+    gold_per_min: number,
+    xp_per_min: number,
+    hero_damage: number,
+    tower_damage: number,
+    hero_healing: number,
+    gold: number,
+    gold_spent: number,
+    scaled_hero_damage: number,
+    scaled_tower_damage: number,
+    scaled_hero_healing: number,
+
+    leaver_status: number,
+    denies: number,
+    level: number,
+    net_worth: number,
+    aghanims_scepter: number,
+    aghanims_shard: number,
+    moonshard: number,
+    ability_upgrades: {
+        ability: number,
+        time: number,
+        level: number
+    }[]
+}
+
+export interface DotaMatchDetails {
+    // can be mapped to the region: https://github.com/kronusme/dota2-api/blob/master/data/regions.json
+    cluster: number,
+
+    players: DotaPlayerMatchDetails[],
+
+    duration: number,
+    pre_game_duration: number,
+    start_time: number, // timestamp in seconds                
+    lobby_type: number,
+    leagueid: number,
+    game_mode: number,
+    first_blood_time: number,
+    human_players: number,
+    positive_votes: number,
+    negative_votes: number,
+    flags: number,
+    engine: number,
+    picks_bans: {
+        is_pick: boolean,
+        hero_id: number,
+        team: number,
+        order: number
+    }[],
+
+    radiant_win: boolean,
+    tower_status_radiant: number,
+    tower_status_dire: number,
+    barracks_status_radiant: number,
+    barracks_status_dire: number,
+    radiant_score: number,
+    dire_score: number,
+
+    // can hide
+    match_id: number,
+    match_seq_num: number,
+}
+
 export default class Dota {
     steamKey?: string;
     openDotaKey?: string;
@@ -97,7 +182,18 @@ export default class Dota {
         }
     }
 
-    async getMatchDetails() {
+    async getMatchDetails(matchId: number): Promise<DotaMatchDetails> {
+        if(this.steamKey) {
+            const query = `?key=${this.steamKey}&match_id=${matchId}`
+            const url = `${this.baseUrl}/IDOTA2Match_570/GetMatchDetails/v001/${query}`
+            const resp = await axios.get(url)
+            if(resp.status !== 200) {
+                throw Error("Request failed: " + resp.status)
+            }
+            return resp.data.result as DotaMatchDetails
+        } else {
+            throw Error("Only steam version is implemented")
+        }
     }
 
     // feed relevant matches (ids or data) and may be player data;
